8f870a96b21579cb795eccb5edf88df7f78a1855
User <williampepple1@gmail.com>
2026-07-05 19:44:59 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
   // Configure basePath if hosting in a subfolder, e.g. basePath: '/dictionary'
 };
 
